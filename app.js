bb66cbf1e376c6009fda1af9fb18d4c71644a2a2
Bryan Arenas Reyes <bryanarenasreyes@MacBook-Pro-025.local>
2023-03-31 19:20:56 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Practicando CD/CI!'));
+app.get('/', (req, res) => res.send('Pipeline is awesome!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
